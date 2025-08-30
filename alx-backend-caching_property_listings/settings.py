e38e4b88b68fad9fec01b7eaea54e920dc263a82
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-08-30 07:05:33 +0100

    Update and rename README.md to alx-backend-caching_property_listings/settings.py

---
+++ alx-backend-caching_property_listings/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'property_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres',
+        'PORT': 5432,
+    }
+}
